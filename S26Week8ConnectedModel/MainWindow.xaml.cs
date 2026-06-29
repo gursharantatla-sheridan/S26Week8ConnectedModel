9e2d06434f7e5a21b16907ed875d568535f1cef3
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-06-29 16:56:51 -0400

    Add project files.

---
+++ S26Week8ConnectedModel/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+
+namespace S26Week8ConnectedModel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
